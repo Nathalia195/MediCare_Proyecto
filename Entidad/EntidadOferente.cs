279933d2ae10949e21daed12ef30957347bd87f9
YARITZIFALLAS\Yaritzi <yarifp214@gmail.com>
2023-05-27 17:29:30 -0600

    j

---
+++ Entidad/EntidadOferente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    internal class EntidadOferente
+    {
+        public string Identificacion { get; set; }
+        public int CodTipoIdentificacion { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string LugardeRecidencia { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public byte Curriculum { get; set; }
+
+       
+    }
+}
