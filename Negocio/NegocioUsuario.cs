635b5ddba421975e00602c5ca5558eb783838501
Nathalia195 <natyr195@gmail.com>
2023-05-28 14:30:01 -0600

    commit yaritzi

--- Negocio/NegocioUsuario.cs
+++ Negocio/NegocioUsuario.cs
@@ -33,21 +33,43 @@ namespace Negocio
         {
             if (string.IsNullOrEmpty(Usuario.NombreUsuario) || string.IsNullOrEmpty(Usuario.Contrasena)) return ""; else return "ok";
         }
-
+        private string ValidarUsuario1(EntidadUsuario Usuario)
+        {
+            if (string.IsNullOrEmpty(Usuario.NombreUsuario)  ||
+                string.IsNullOrEmpty(Usuario.NombreCompleto) ||
+                string.IsNullOrEmpty(Usuario.CorreoElectronico) ||  
+                string.IsNullOrEmpty(Usuario.Contrasena)   ||
+                string.IsNullOrEmpty(Usuario.Imagen.ToString()))
+                return "Debe de completar todos los campos"; 
+                else return "";
+        }
         public List<EntidadUsuario> GetUsuario()
         {
             return obj_datos.GetUsuario();
         }
 
+        public void BloquearUsuario(EntidadUsuario usuario)
+        {
+            obj_datos.BloquearUsuario(usuario);
+        }
 
-        //public void InsertarUsuarios(EntidadUsuario mcEntidad)
-        //{
-        //    obj_datos.InsertarUsuario(mcEntidad);
-        //}
-        //public void Editar(EntidadUsuario mcEntidad)
-        //{
-        //    obj_datos.EditarUsuario(mcEntidad);
-        //}
+
+        public string InsertarUsuarios(EntidadUsuario mcEntidad)
+        {
+            string mensaje = ValidarUsuario1(mcEntidad);
+
+            if (string.IsNullOrEmpty(mensaje))
+            {
+                return obj_datos.InsertarUsuarios(mcEntidad);
+            }
+            else
+            {
+                return mensaje;
+            }
+
+
+
+        }
 
     }
 }
