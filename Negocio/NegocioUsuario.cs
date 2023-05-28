663e046c213fd3f096649ecf2fc5514ab8c92a62
Nathalia195 <natyr195@gmail.com>
2023-05-28 14:48:53 -0600

    ggCrud, bloqueo, avatar de usuarios

--- Negocio/NegocioUsuario.cs
+++ Negocio/NegocioUsuario.cs
@@ -25,7 +25,6 @@ namespace Negocio
         {
             if (string.IsNullOrEmpty(Usuario.NombreUsuario) || string.IsNullOrEmpty(Usuario.Contrasena)) return ""; else return "ok";
         }
-<<<<<<< HEAD
         private string ValidarUsuario1(EntidadUsuario Usuario)
         {
             if (string.IsNullOrEmpty(Usuario.NombreUsuario)  ||
@@ -36,12 +35,8 @@ namespace Negocio
                 return "Debe de completar todos los campos"; 
                 else return "";
         }
-=======
 
-        private string ValidarUsuario1(EntidadUsuario Usuario)
-        {
-            if (string.IsNullOrEmpty(Usuario.NombreUsuario) || string.IsNullOrEmpty(Usuario.NombreCompleto) || string.IsNullOrEmpty(Usuario.CorreoElectronico) || string.IsNullOrEmpty(Usuario.Contrasena) ||  string.IsNullOrEmpty(Usuario.Imagen.ToString()))return "Debe de completar todos los campos"; else return "";
-        }
+      
 
         public byte[] TraerAvatar(EntidadUsuario Usuario)
         {
@@ -49,54 +44,41 @@ namespace Negocio
         }
 
 
->>>>>>> 99b6111 (Crud, bloqueo, avatar de usuarios)
         public List<EntidadUsuario> GetUsuario()
         {
             return obj_datos.GetUsuario();
         }
+        
+
         public void BloquearUsuario(EntidadUsuario usuario)
         {
             obj_datos.BloquearUsuario(usuario);
         }
 
-<<<<<<< HEAD
-        public void BloquearUsuario(EntidadUsuario usuario)
-        {
-            obj_datos.BloquearUsuario(usuario);
-        }
 
-
+      
         public string InsertarUsuarios(EntidadUsuario mcEntidad)
         {
-=======
 
-        public string InsertarUsuarios(EntidadUsuario mcEntidad)
-        {
->>>>>>> 99b6111 (Crud, bloqueo, avatar de usuarios)
             string mensaje = ValidarUsuario1(mcEntidad);
 
             if (string.IsNullOrEmpty(mensaje))
             {
-<<<<<<< HEAD
+
                 return obj_datos.InsertarUsuarios(mcEntidad);
-=======
-                return   obj_datos.InsertarUsuarios(mcEntidad);
->>>>>>> 99b6111 (Crud, bloqueo, avatar de usuarios)
+
+              
             }
             else
             {
                 return mensaje;
             }
-<<<<<<< HEAD
 
 
 
-=======
            
 
-              
->>>>>>> 99b6111 (Crud, bloqueo, avatar de usuarios)
-        }
+                }
 
 
 
